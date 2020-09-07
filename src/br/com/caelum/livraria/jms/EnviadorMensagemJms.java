b388067e61afe85027da2def90cf98ed23a3ab6e
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-09-07 20:06:33 -0300

    ex5.22

--- src/br/com/caelum/livraria/jms/EnviadorMensagemJms.java
+++ src/br/com/caelum/livraria/jms/EnviadorMensagemJms.java
@@ -2,10 +2,15 @@ package br.com.caelum.livraria.jms;
 
 import java.io.Serializable;
 
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.JMSProducer;
+import javax.jms.Topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
-import br.com.caelum.livraria.jaxb.SerializadorXml;
 import br.com.caelum.livraria.modelo.Pedido;
 
 @Component
@@ -14,7 +19,18 @@ public class EnviadorMensagemJms implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Autowired
+	private ConnectionFactory factory;
+
+	@Autowired
+	private Topic topico;
+
 	public void enviar(Pedido pedido) {
-		// código para enviar a mensagem
+		System.out.println("JMS: Enviando pedido:" + pedido);
+		try (JMSContext context = factory.createContext("jms", "jms2")) {
+			JMSProducer producer = context.createProducer();
+			producer.setProperty("formato", pedido.getFormato());
+			producer.send(topico, pedido.toString());
+		}
 	}
 }
